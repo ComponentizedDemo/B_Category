a294dce5f896b8a545b717f64971dadb49bb200f
qihang <854883819@qq.com>
2019-10-16 19:57:11 +0800

    4

--- B_Category/B_Category/CTMediator+B.h
+++ B_Category/B_Category/CTMediator+B.h
@@ -2,12 +2,13 @@
 //  CTMediator+B.h
 //  B_Category
 //
-//  Created by 赵希帆 on 2017/8/16.
-//  Copyright © 2017年 赵希帆. All rights reserved.
+//  Created by  on 2017/8/16.
+//  Copyright © 2017年 . All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CTMediator/CTMediator.h>
+
 
 @interface CTMediator (B)
 
