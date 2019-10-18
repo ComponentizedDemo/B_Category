1c0edd37c57ba02a3bccc254a55c3f2c18eaf7e1
qihang <854883819@qq.com>
2019-10-18 18:43:44 +0800

    m

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by  on 2017/8/16.
+//  Copyright © 2017年 . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CTMediator/CTMediator.h>
+
+
+@interface CTMediator (B)
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
---
+++ README.md
@@ -0,0 +1,5 @@
+使用fastlane快速发布pods
+
+```ruby
+fastlane release_pod repo:ZYSpec project:'框架名' version:'版本'
+```
